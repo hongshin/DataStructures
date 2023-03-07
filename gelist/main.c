8aff091621a956d2c572f254bbf82e660e942488
Shin Hong <hongshin@gmail.com>
2023-03-07 11:18:44 +0900

    Update gelist

--- gelist/main.c
+++ gelist/main.c
@@ -3,13 +3,17 @@
 
 int main ()
 {
+	gentry e1 = {"Rob", 750}, e2 = {"Jack", 510}, e3 = {"Mike", 1105}, e4 = {"Paul", 720} ;
+
 	gelist * l ;
 
 	l = gelist_alloc(5) ;
 
 
-	gentry e1 = {"Rob", 750}, e2 = {"Jack", 510}, e3 = {"Mike", 1105}, e4 = {"Paul", 720} ;
-
+	gelist_add(l, e1) ;
+	gelist_add(l, e2) ;
+	gelist_add(l, e3) ;
+	gelist_add(l, e4) ;
 	gelist_add(l, e1) ;
 	gelist_add(l, e2) ;
 	gelist_add(l, e3) ;
