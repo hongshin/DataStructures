b32fad10562647f5f5981fbca053086343f36a12
hongshin <hongshin@gmail.com>
2024-03-18 21:26:40 +0900

    Update poly.c

--- code/Mar15/poly.c
+++ code/Mar15/poly.c
@@ -1,41 +1,132 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct term_t {
-	int exp ;
-	double coef ;
-} ;
+typedef 
+	struct {
+		int exp ;
+		double coef ;
+	} 
+	term_t ;
 
-struct poly_t {
-	int degree ;
-	term_t * terms ;
-} ;
+typedef 
+	struct {
+		int degree ;
+		int n_terms ;
+		term_t * terms ; // a list of attached terms 
+	} 
+	poly_t ;
 
 poly_t * poly_zero () 
 {
+	poly_t * p = (poly_t *) malloc(sizeof(poly_t)) ;
+	
+	p->degree = 0 ;
+	p->n_terms = 0 ;
+	p->terms = NULL ;
+	return p ; 
 }
 
 int poly_degree (poly_t * p)
 {
+	return p->degree ;
 }
 
-double poly_coef (poly_t * p)
+double poly_coef (poly_t * p, int exp)
 {
+	if (exp < 0)
+		return 0.0 ;
+	if (p->degree < exp)
+		return 0.0 ;
+
+	int i ;
+	for (i = 0 ; i < p->n_terms ; i++) {
+		if ((p->terms[i]).exp == exp) {
+			return (p->terms[i]).coef ;
+		}
+	}
+	return 0.0 ;
+}	
+
+poly_t * poly_attach (poly_t * p, double coef, int exp)
+{
+	if (exp < 0)
+		return NULL ;
+	if (coef == 0.0)
+		return NULL ;
+	if (poly_coef(p, exp) != 0.0)
+		return NULL ;
+
+	if (p->degree < exp) {
+		p->degree = exp ;
+	}
+	p->terms = realloc(p->terms, sizeof(term_t) * (p->n_terms + 1)) ; 
+	(p->terms[p->n_terms]).coef = coef ;
+	(p->terms[p->n_terms]).exp = exp ;
+	p->n_terms += 1 ;
+
+	return p ;
 }
 
-poly_t poly_attach (poly_t * p)
+poly_t * poly_remove (poly_t * p, int exp)
 {
-}
+	if (exp < 0)
+		return NULL ;
+	if (p->degree < exp)
+		return NULL ;
+	if (poly_coef(p, exp) == 0.0)
+		return NULL ;
 
-poly_t poly_remove (poly_t * p)
-{
+	int i = 0 ;
+	while (p->terms[i].exp != exp) {
+		i++ ;
+	}
+	// assert p->terms[i].exp == exp
+
+	for ( ; i < p->n_terms - 1 ; i++) {
+		p->terms[i] = p->terms[i + 1] ;
+	}
+	p->terms = realloc(p->terms, (p->n_terms - 1) * sizeof(term_t)) ;
+	p->n_terms -= 1 ;
+
+	int j ;
+	p->degree = 0 ;
+	for (j = 0 ; j < p->n_terms ; j++) {
+		if (p->degree < p->terms[j].exp) {
+			p->degree = p->terms[j].exp ;
+		}
+	}
+
+	return p ;
 }
 
 void poly_show (poly_t * p)
 {
+	int i ;
+	for (i = 0 ; i < p->n_terms ; i++) {
+		printf("%lf", p->terms[i].coef) ;
+		if (p->terms[i].exp != 0) {
+			printf("x^%d", p->terms[i].exp) ;
+		}
+		if (i != p->n_terms - 1) {
+			printf("+") ;
+		}
+		printf(" ") ;
+	}
+	printf("\n") ;
 }
 
 int main ()
 {
+	//1.0x^2 + 2.0x+ 1.0 
+	poly_t * p = poly_zero() ;
+	printf("%d\n", p == poly_attach(p, 1.0, 2)) ;
+	printf("%d\n", p == poly_attach(p, 2.0, 1)) ;
+	printf("%d\n", p == poly_attach(p, 1.0, 0)) ;
+	printf("%d\n", p == poly_attach(p, 2.0, 2)) ;
+	poly_show(p) ;
+
+	printf("%d\n", p == poly_remove(p, 1)) ;
+	poly_show(p) ;
+
 	return EXIT_SUCCESS ;
 }
