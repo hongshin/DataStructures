8d32ce093cf1c9abbeccb7919bc61bf5847f2841
Shin Hong <hongshin@gmail.com>
2024-05-03 12:27:53 +0900

    add homework 4

---
+++ assignments/homework4/lalist.h
@@ -0,0 +1,54 @@
+typedef 
+	struct _lalist_node {
+		void * arr ;
+		int n_elem ;
+		struct _lalist_node * next ;
+	} 
+	lalist_node_t ;
+
+typedef 
+	struct {
+		int arr_cap ;
+		int elem_size ;
+		lalist_node_t * first ;
+	} 
+	lalist_t ;
+
+lalist_t *
+lalist_alloc (int arr_cap, int elem_size)  ;
+
+void
+lalist_free (lalist_t * l) ;
+
+int 
+lalist_length (lalist_t * l) ;
+
+void
+lalist_insert_first (lalist_t * l, void * e) ;
+
+void
+lalist_insert_last (lalist_t * l, void * e) ;
+
+int
+lalist_insert_at (lalist_t * l, int idx, void * e) ;
+
+int
+lalist_remove (lalist_t * l, lalist_node_t * n) ;
+
+int
+lalist_remove_first (lalist_t * l, void * e) ;
+
+int
+lalist_remove_last (lalist_t * l, void * e) ;
+
+int
+lalist_remove_at (lalist_t * l, int idx, void * e) ;
+
+void
+lalist_apply (lalist_t * l1, void (* f)(void * e)) ;
+
+void 
+lalist_info (lalist_t * l) ;
+
+void
+lalist_pack (lalist_t * l) ;
---
+++ assignments/homework4/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lalist.h"
+
+void print_element (void * e)
+{
+	int * a ; 
+
+	a = (int *) e ;
+
+	printf("%d ", *a) ;
+}
+
+void double_element (void * e)
+{
+	/* TODO */
+}
+
+
+int main () 
+{
+	int data ;
+	lalist_t * l ; 
+
+	l = lalist_alloc(3, sizeof(int)) ;
+
+	data = 1 ;
+	lalist_insert_last(l, &data) ;
+
+	data = 2 ;
+	lalist_insert_last(l, &data) ;
+
+	data = 3 ;
+	lalist_insert_last(l, &data) ;
+
+	data = 4 ;
+	lalist_insert_first(l, &data) ; 
+
+	data = 5 ;
+	lalist_insert_last(l, &data) ;
+
+	data = 6 ;
+	lalist_insert_at (l, 2, &data) ;
+
+	data = 7 ;
+	lalist_insert_at(l, 3, &data) ;
+
+	data = 8 ;
+	lalist_insert_last(l, &data) ;
+
+	lalist_apply(l, print_element) ; 
+	printf("\n") ;
+
+	lalist_info(l) ;
+
+	lalist_remove_first(l, &data) ; 
+	lalist_remove_last(l, &data) ;
+	lalist_remove_last(l, &data) ;
+
+	lalist_apply(l, print_element) ; 
+	printf("\n") ;
+	lalist_info(l) ;
+
+	lalist_pack(l) ;
+	lalist_apply(l, print_element) ; 
+	printf("\n") ;
+	lalist_info(l) ;
+
+	lalist_free(l) ;
+
+	return EXIT_SUCCESS ;
+}
